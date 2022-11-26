7ab5c529757a82785c345dfb647bea61f5d54d86
zengchen1024 <chenzeng2@huawei.com>
2022-11-26 19:59:19 +0800

    watch the training if it is timeout

--- infrastructure/watchimpl/config.go
+++ infrastructure/watchimpl/config.go
@@ -5,11 +5,19 @@ type Config struct {
 	// that check all trainings in a loop.
 	Interval int `json:"interval"`
 
+	// Timeout specifies the time that a training can live
+	// The unit is second.
+	Timeout int `json:"timeout"`
+
 	Endpoint string `json:"endpoint" required:"true"`
 }
 
 func (cfg *Config) SetDefault() {
 	if cfg.Interval <= 0 {
 		cfg.Interval = 10
+	}
+
+	if cfg.Timeout <= 0 {
+		cfg.Timeout = 86400
 	}
 }
